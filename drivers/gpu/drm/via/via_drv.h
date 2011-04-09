d15b4eaea905aa5c33ed25948b857766b4244189
James Simmons <jsimmons@infradead.org>
2011-04-09 16:40:34 -0400

    add filler for future cpi dma backend

--- drivers/gpu/drm/via/via_drv.h
+++ drivers/gpu/drm/via/via_drv.h
@@ -157,6 +157,8 @@ extern int via_detect_vram(struct drm_device *dev);
 
 extern int via_ttm_init(struct drm_via_private *dev_priv);
 extern void via_ttm_bo_destroy(struct ttm_buffer_object *bo);
+extern struct ttm_backend *ttm_pci_backend_init(struct ttm_bo_device *bdev,
+						struct drm_device *dev);
 
 extern int ttm_global_init(struct drm_global_reference *global_ref,
 				struct ttm_bo_global_ref *global_bo,
