ee5454fc864b540f4f0ac45aed8328c04de0265c
Kevin Brace <kevinbrace@gmx.com>
2017-03-02 20:31:33 -0800

    Version bumped to 3.0.24
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/via/via_drv.h
+++ drivers/gpu/drm/via/via_drv.h
@@ -30,11 +30,11 @@
 #define DRIVER_AUTHOR       "The OpenChrome Project"
 #define DRIVER_NAME         "via"
 #define DRIVER_DESC         "OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE         "20170127"
+#define DRIVER_DATE         "20170302"
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		0
-#define DRIVER_PATCHLEVEL	23
+#define DRIVER_PATCHLEVEL	24
 
 #include <linux/module.h>
 
