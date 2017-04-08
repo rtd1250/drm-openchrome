cc4d84aaa7514e7a35cb6c0e870c8564292b7679
Kevin Brace <kevinbrace@gmx.com>
2017-04-07 18:55:27 -0700

    Added viaAnalogSetDisplaySource
    This inline function was added to crtc_hw.h.
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/via/crtc_hw.h
+++ drivers/gpu/drm/via/crtc_hw.h
@@ -122,6 +122,21 @@ viaAnalogSetSyncPolarity(void __iomem *regs, u8 syncPolarity)
 }
 
 /*
+ * Sets analog (VGA) display source.
+ */
+static inline void
+viaAnalogSetDisplaySource(void __iomem *regs, u8 displaySource)
+{
+	/* Set analog (VGA) display source. */
+	/* 3C5.16[6] - CRT Display Source
+	 *             0: Primary Display Stream (IGA1)
+	 *             1: Secondary Display Stream (IGA2) */
+	svga_wseq_mask(regs, 0x16, displaySource << 6, BIT(6));
+	DRM_DEBUG_KMS("Analog (VGA) Display Source: IGA%d\n",
+			(displaySource & 0x01) + 1);
+}
+
+/*
  * Sets CX700 or later single chipset's LVDS1 power sequence type.
  */
 static inline void
