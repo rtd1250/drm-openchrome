4e40895ddc19e115ae4c3bb630f02efaf147706d
Kevin Brace <kevinbrace@gmx.com>
2017-04-15 14:44:43 -0700

    Version bumped to 3.0.31
    This version creates a dedicated mode setting callback function for
    CX700 / VX700 and VX800 chipsets’ integrated TMDS transmitter.
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/via/via_drv.h
+++ drivers/gpu/drm/via/via_drv.h
@@ -30,11 +30,11 @@
 #define DRIVER_AUTHOR       "The OpenChrome Project"
 #define DRIVER_NAME         "via"
 #define DRIVER_DESC         "OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE         "20170411"
+#define DRIVER_DATE         "20170415"
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		0
-#define DRIVER_PATCHLEVEL	30
+#define DRIVER_PATCHLEVEL	31
 
 #include <linux/module.h>
 
