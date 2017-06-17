rtd1250/drm-openchrome

commit bf8d577aedd3c3d5fa9f2bf24d6ca3a046005542

Controlling analog (VGA) power state from DPMS callback function
Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

drivers/gpu/drm/via/via_analog.c
@@ -106,15 +106,19 @@ via_analog_dpms(struct drm_encoder *encoder, int mode)
 	switch (mode) {
 	case DRM_MODE_DPMS_ON:
 		via_analog_set_dpms_control(VGABASE, VIA_ANALOG_DPMS_ON);
+		via_analog_power(VGABASE, true);
 		break;
 	case DRM_MODE_DPMS_STANDBY:
 		via_analog_set_dpms_control(VGABASE, VIA_ANALOG_DPMS_STANDBY);
+		via_analog_power(VGABASE, true);
 		break;
 	case DRM_MODE_DPMS_SUSPEND:
 		via_analog_set_dpms_control(VGABASE, VIA_ANALOG_DPMS_SUSPEND);
+		via_analog_power(VGABASE, true);
 		break;
 	case DRM_MODE_DPMS_OFF:
 		via_analog_set_dpms_control(VGABASE, VIA_ANALOG_DPMS_OFF);
+		via_analog_power(VGABASE, false);
 		break;
 	default:
 		DRM_ERROR("Bad DPMS mode.");

commit c33183e5d2b4c5bc16d84001d3c2b81b1ec10605

Correcting a bug that was introduced with commit bf8d577
This bad commit leading to a kernel panic during boot.

Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

drivers/gpu/drm/via/via_analog.c
@@ -106,19 +106,19 @@ via_analog_dpms(struct drm_encoder *encoder, int mode)
 	switch (mode) {
 	case DRM_MODE_DPMS_ON:
 		via_analog_set_dpms_control(VGABASE, VIA_ANALOG_DPMS_ON);
-		via_analog_power(VGABASE, true);
+		via_analog_power(dev_priv, true);
 		break;
 	case DRM_MODE_DPMS_STANDBY:
 		via_analog_set_dpms_control(VGABASE, VIA_ANALOG_DPMS_STANDBY);
-		via_analog_power(VGABASE, true);
+		via_analog_power(dev_priv, true);
 		break;
 	case DRM_MODE_DPMS_SUSPEND:
 		via_analog_set_dpms_control(VGABASE, VIA_ANALOG_DPMS_SUSPEND);
-		via_analog_power(VGABASE, true);
+		via_analog_power(dev_priv, true);
 		break;
 	case DRM_MODE_DPMS_OFF:
 		via_analog_set_dpms_control(VGABASE, VIA_ANALOG_DPMS_OFF);
-		via_analog_power(VGABASE, false);
+		via_analog_power(dev_priv, false);
 		break;
 	default:
 		DRM_ERROR("Bad DPMS mode.");
@@ -169,7 +169,7 @@ via_analog_prepare(struct drm_encoder *encoder)
 
 	if (encoder->crtc) {
 		via_analog_set_dpms_control(VGABASE, VIA_ANALOG_DPMS_OFF);
-		via_analog_power(VGABASE, false);
+		via_analog_power(dev_priv, false);
 	}
 
 	DRM_DEBUG_KMS("Exiting via_analog_prepare.\n");
@@ -184,7 +184,7 @@ via_analog_commit(struct drm_encoder *encoder)
 
 	if (encoder->crtc) {
 		via_analog_set_dpms_control(VGABASE, VIA_ANALOG_DPMS_ON);
-		via_analog_power(VGABASE, true);
+		via_analog_power(dev_priv, true);
 	}
 
 	DRM_DEBUG_KMS("Exiting via_analog_commit.\n");