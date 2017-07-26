34c86ff3521cee92a4c08d6d95262e8cdcc64242
Kevin Brace <kevinbrace@gmx.com>
2017-07-25 20:40:03 -0700

    Version bumped to 3.0.42
    Changed the DRM module name from via to openchrome. This allows
    (potential) coexistence with VIA Technologies’ proprietary (and
    non-mainlined) Chrome9 DRM.
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/via_drv.h
+++ drivers/gpu/drm/openchrome/via_drv.h
@@ -30,11 +30,11 @@
 #define DRIVER_AUTHOR       "The OpenChrome Project"
 #define DRIVER_NAME         "openchrome"
 #define DRIVER_DESC         "OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE         "20170712"
+#define DRIVER_DATE         "20170725"
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		0
-#define DRIVER_PATCHLEVEL	41
+#define DRIVER_PATCHLEVEL	42
 
 #include <linux/module.h>
 
