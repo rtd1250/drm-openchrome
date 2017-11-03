6215de0acc85d2c989a04325115c54485da832c1
Kevin Brace <kevinbrace@gmx.com>
2017-11-03 15:38:46 -0700

    Version bumped to 3.0.52
    Made improvements in how I2C bus is used to detect the presence
    of a display device.
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/via_drv.h
+++ drivers/gpu/drm/openchrome/via_drv.h
@@ -30,11 +30,11 @@
 #define DRIVER_AUTHOR       "OpenChrome Project"
 #define DRIVER_NAME         "openchrome"
 #define DRIVER_DESC         "OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE         "20171027"
+#define DRIVER_DATE         "20171103"
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		0
-#define DRIVER_PATCHLEVEL	51
+#define DRIVER_PATCHLEVEL	52
 
 #include <linux/module.h>
 
