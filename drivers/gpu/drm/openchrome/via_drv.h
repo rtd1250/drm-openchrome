b9ad58754850d0bd5d2e45cf7c8c528b7f7f3470
Kevin Brace <kevinbrace@gmx.com>
2018-01-28 14:45:19 -0800

    drm/openchrome: Version bumped to 3.0.67
    Display FIFO fixes for P4M800 Pro chipset. Another small adjustment
    (PREQ / TREQ control bit) for UniChrome and UniChrome Pro devices.
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/via_drv.h
+++ drivers/gpu/drm/openchrome/via_drv.h
@@ -30,11 +30,11 @@
 #define DRIVER_AUTHOR       "OpenChrome Project"
 #define DRIVER_NAME         "openchrome"
 #define DRIVER_DESC         "OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE         "20180127"
+#define DRIVER_DATE         "20180128"
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		0
-#define DRIVER_PATCHLEVEL	66
+#define DRIVER_PATCHLEVEL	67
 
 #include <linux/module.h>
 
