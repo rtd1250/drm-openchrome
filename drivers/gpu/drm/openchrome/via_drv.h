e3eb41c79815b57fb447ba6499e69052d21684fc
Kevin Brace <kevinbrace@gmx.com>
2018-02-13 17:07:33 -0800

    drm/openchrome: Version bumped to 3.0.76
    Adds I2C bus 2 support for analog (VGA) output. This will now allow
    DVI to VGA passive converter to work properly in most cases.
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/via_drv.h
+++ drivers/gpu/drm/openchrome/via_drv.h
@@ -30,11 +30,11 @@
 #define DRIVER_AUTHOR       "OpenChrome Project"
 #define DRIVER_NAME         "openchrome"
 #define DRIVER_DESC         "OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE         "20180211"
+#define DRIVER_DATE         "20180213"
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		0
-#define DRIVER_PATCHLEVEL	75
+#define DRIVER_PATCHLEVEL	76
 
 #include <linux/module.h>
 
