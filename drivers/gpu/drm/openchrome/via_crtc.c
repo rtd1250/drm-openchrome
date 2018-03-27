rtd1250/drm-openchrome

commit 981d27b3a95a7df6fb6c6887b467f576bf8a8b97

drm/openchrome: Hardware cursor fix for single HI devices
Older generation VIA Technologies Chrome IGP devices have only one
hardware icon (HI). Due to the way the code was written previously, it
was assuming the availability of two HIs, and as a result, it the user
was using older generation devices, no mouse cursor will be displayed
on the screen. This commit will add the necessary code to support older
devices with only one HI. The code was validated on ECS K8M890M-M
mainboard with K8M890 chipset.

Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

drivers/gpu/drm/openchrome/via_crtc.c
@@ -151,49 +151,141 @@ static void via_iga2_set_color_depth(struct via_device *dev_priv,
 
 static void via_hide_cursor(struct drm_crtc *crtc)
 {
+	struct drm_device *dev = crtc->dev;
 	struct via_crtc *iga = container_of(crtc, struct via_crtc, base);
 	struct via_device *dev_priv = crtc->dev->dev_private;
 	uint32_t temp;
 
-	if (iga->index) {
+	DRM_DEBUG_KMS("Entered %s.\n", __func__);
+
+	switch (dev->pdev->device) {
+	case PCI_DEVICE_ID_VIA_PM800:
+	case PCI_DEVICE_ID_VIA_CX700:
+	case PCI_DEVICE_ID_VIA_P4M890:
+	case PCI_DEVICE_ID_VIA_P4M900:
+	case PCI_DEVICE_ID_VIA_VX800:
+	case PCI_DEVICE_ID_VIA_VX855:
+	case PCI_DEVICE_ID_VIA_VX900:
+		if (iga->index) {
+			temp = VIA_READ(HI_CONTROL);
+			VIA_WRITE(HI_CONTROL, temp & 0xFFFFFFFA);
+		} else {
+			temp = VIA_READ(PRIM_HI_CTRL);
+			VIA_WRITE(PRIM_HI_CTRL, temp & 0xFFFFFFFA);
+		}
+
+		break;
+	default:
 		temp = VIA_READ(HI_CONTROL);
 		VIA_WRITE(HI_CONTROL, temp & 0xFFFFFFFA);
-	} else {
-		temp = VIA_READ(PRIM_HI_CTRL);
-		VIA_WRITE(PRIM_HI_CTRL, temp & 0xFFFFFFFA);
+		break;
 	}
+
+	DRM_DEBUG_KMS("Exiting %s.\n", __func__);
 }
 
 static void via_show_cursor(struct drm_crtc *crtc)
 {
+	struct drm_device *dev = crtc->dev;
 	struct via_crtc *iga = container_of(crtc, struct via_crtc, base);
 	struct via_device *dev_priv = crtc->dev->dev_private;
 
-	if (!iga->cursor_kmap.bo)
-		return;
+	DRM_DEBUG_KMS("Entered %s.\n", __func__);
 
-	/* Program the offset and turn on Hardware icon Cursor */
-	if (iga->index) {
-		VIA_WRITE(HI_FBOFFSET, iga->cursor_kmap.bo->offset);
-		VIA_WRITE(HI_CONTROL, 0xB6000005);
-	} else {
-		VIA_WRITE(PRIM_HI_FBOFFSET, iga->cursor_kmap.bo->offset);
-		VIA_WRITE(PRIM_HI_CTRL, 0x36000005);
-	}
+	switch (dev->pdev->device) {
+	case PCI_DEVICE_ID_VIA_PM800:
+	case PCI_DEVICE_ID_VIA_CX700:
+	case PCI_DEVICE_ID_VIA_P4M890:
+	case PCI_DEVICE_ID_VIA_P4M900:
+	case PCI_DEVICE_ID_VIA_VX800:
+	case PCI_DEVICE_ID_VIA_VX855:
+	case PCI_DEVICE_ID_VIA_VX900:
+		/* Program Hardware Icon (HI) FIFO, foreground, and
+		 * background colors. */
+		if (iga->index) {
+			VIA_WRITE(HI_TRANSPARENT_COLOR, 0x00000000);
+			VIA_WRITE(HI_INVTCOLOR, 0x00FFFFFF);
+			VIA_WRITE(ALPHA_V3_PREFIFO_CONTROL, 0x000E0000);
+			VIA_WRITE(ALPHA_V3_FIFO_CONTROL, 0x0E0F0000);
+		} else {
+			VIA_WRITE(PRIM_HI_TRANSCOLOR, 0x00000000);
+			VIA_WRITE(PRIM_HI_INVTCOLOR, 0x00FFFFFF);
+			VIA_WRITE(V327_HI_INVTCOLOR, 0x00FFFFFF);
+			VIA_WRITE(PRIM_HI_FIFO, 0x0D000D0F);
+		}
 
-	/* Program Hardware Icon (HI) FIFO and foreground
-	 * and background colors. */
-	if (iga->index) {
+		break;
+	default:
 		VIA_WRITE(HI_TRANSPARENT_COLOR, 0x00000000);
 		VIA_WRITE(HI_INVTCOLOR, 0x00FFFFFF);
 		VIA_WRITE(ALPHA_V3_PREFIFO_CONTROL, 0x000E0000);
-		VIA_WRITE(ALPHA_V3_FIFO_CONTROL, 0x0E0F0000);
-	} else {
-		VIA_WRITE(PRIM_HI_TRANSCOLOR, 0x00000000);
-		VIA_WRITE(PRIM_HI_INVTCOLOR, 0x00FFFFFF);
-		VIA_WRITE(V327_HI_INVTCOLOR, 0x00FFFFFF);
-		VIA_WRITE(PRIM_HI_FIFO, 0x0D000D0F);
+		VIA_WRITE(ALPHA_V3_FIFO_CONTROL, 0xE0F0000);
+		break;
 	}
+
+	switch (dev->pdev->device) {
+	case PCI_DEVICE_ID_VIA_PM800:
+	case PCI_DEVICE_ID_VIA_CX700:
+	case PCI_DEVICE_ID_VIA_P4M890:
+	case PCI_DEVICE_ID_VIA_P4M900:
+	case PCI_DEVICE_ID_VIA_VX800:
+	case PCI_DEVICE_ID_VIA_VX855:
+	case PCI_DEVICE_ID_VIA_VX900:
+		/* Turn on Hardware icon Cursor */
+		if (iga->index) {
+			VIA_WRITE(HI_CONTROL, 0xB6000005);
+		} else {
+			VIA_WRITE(PRIM_HI_CTRL, 0x36000005);
+		}
+
+		break;
+	default:
+		if (iga->index) {
+			VIA_WRITE(HI_CONTROL, 0xB6000005);
+		} else {
+			VIA_WRITE(HI_CONTROL, 0x36000005);
+		}
+
+		break;
+	}
+
+	DRM_DEBUG_KMS("Exiting %s.\n", __func__);
+}
+
+static void via_cursor_address(struct drm_crtc *crtc)
+{
+	struct drm_device *dev = crtc->dev;
+	struct via_crtc *iga = container_of(crtc, struct via_crtc, base);
+	struct via_device *dev_priv = crtc->dev->dev_private;
+
+	DRM_DEBUG_KMS("Entered %s.\n", __func__);
+
+	if (!iga->cursor_kmap.bo) {
+		goto exit;
+	}
+
+	switch (dev->pdev->device) {
+	case PCI_DEVICE_ID_VIA_PM800:
+	case PCI_DEVICE_ID_VIA_CX700:
+	case PCI_DEVICE_ID_VIA_P4M890:
+	case PCI_DEVICE_ID_VIA_P4M900:
+	case PCI_DEVICE_ID_VIA_VX800:
+	case PCI_DEVICE_ID_VIA_VX855:
+	case PCI_DEVICE_ID_VIA_VX900:
+		/* Program the HI offset. */
+		if (iga->index) {
+			VIA_WRITE(HI_FBOFFSET, iga->cursor_kmap.bo->offset);
+		} else {
+			VIA_WRITE(PRIM_HI_FBOFFSET, iga->cursor_kmap.bo->offset);
+		}
+		break;
+	default:
+		VIA_WRITE(HI_FBOFFSET, iga->cursor_kmap.bo->offset);
+		break;
+	}
+
+exit:
+	DRM_DEBUG_KMS("Exiting %s.\n", __func__);
 }
 
 static int via_crtc_cursor_set(struct drm_crtc *crtc,
@@ -206,6 +298,8 @@ static int via_crtc_cursor_set(struct drm_crtc *crtc,
 	struct drm_device *dev = crtc->dev;
 	struct drm_gem_object *obj = NULL;
 	struct ttm_bo_kmap_obj user_kmap;
+
+	DRM_DEBUG_KMS("Entered %s.\n", __func__);
 
 	if (!iga->cursor_kmap.bo)
 		return -ENXIO;
@@ -250,12 +344,16 @@ static int via_crtc_cursor_set(struct drm_crtc *crtc,
 		ttm_bo_kunmap(&user_kmap);
 	}
 	drm_gem_object_unreference_unlocked(obj);
+	via_cursor_address(crtc);
 	via_show_cursor(crtc);
+
+	DRM_DEBUG_KMS("Exiting %s.\n", __func__);
 	return ret;
 }
 
 static int via_crtc_cursor_move(struct drm_crtc *crtc, int x, int y)
 {
+	struct drm_device *dev = crtc->dev;
 	struct via_crtc *iga = container_of(crtc, struct via_crtc, base);
 	struct via_device *dev_priv = crtc->dev->dev_private;
 	unsigned char xoff = 0, yoff = 0;
@@ -271,12 +369,27 @@ static int via_crtc_cursor_move(struct drm_crtc *crtc, int x, int y)
 		ypos = 0;
 	}
 
-	if (iga->index) {
+	switch (dev->pdev->device) {
+	case PCI_DEVICE_ID_VIA_PM800:
+	case PCI_DEVICE_ID_VIA_CX700:
+	case PCI_DEVICE_ID_VIA_P4M890:
+	case PCI_DEVICE_ID_VIA_P4M900:
+	case PCI_DEVICE_ID_VIA_VX800:
+	case PCI_DEVICE_ID_VIA_VX855:
+	case PCI_DEVICE_ID_VIA_VX900:
+		if (iga->index) {
+			VIA_WRITE(HI_POSSTART, ((xpos << 16) | (ypos & 0x07ff)));
+			VIA_WRITE(HI_CENTEROFFSET, ((xoff << 16) | (yoff & 0x07ff)));
+		} else {
+			VIA_WRITE(PRIM_HI_POSSTART, ((xpos << 16) | (ypos & 0x07ff)));
+			VIA_WRITE(PRIM_HI_CENTEROFFSET, ((xoff << 16) | (yoff & 0x07ff)));
+		}
+
+		break;
+	default:
 		VIA_WRITE(HI_POSSTART, ((xpos << 16) | (ypos & 0x07ff)));
 		VIA_WRITE(HI_CENTEROFFSET, ((xoff << 16) | (yoff & 0x07ff)));
-	} else {
-		VIA_WRITE(PRIM_HI_POSSTART, ((xpos << 16) | (ypos & 0x07ff)));
-		VIA_WRITE(PRIM_HI_CENTEROFFSET, ((xoff << 16) | (yoff & 0x07ff)));
+		break;
 	}
 
 	return 0;

commit ad7ae852aba77aee36d67640028e218c0afcbd94

drm/openchrome: Fix for corrupted cursor(s) after standby resume
This commit fixes corrupted hardware cursor(s) after standby resume
likely introduced by commit 981d27b3a95a7df6fb6c6887b467f576bf8a8b97.
Furthermore, it also fixes a bug that causes hardware cursor(s) to stay
displayed after switching over to VT (Virtual Terminal). That being
said, this commit will cause a minor regression of turning off the
hardware cursor(s) right after standby resume, although they will be
displayed again when the input device is moved around the screen.

Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

drivers/gpu/drm/openchrome/via_crtc.c
@@ -392,6 +392,8 @@ static int via_crtc_cursor_move(struct drm_crtc *crtc, int x, int y)
 		break;
 	}
 
+	via_show_cursor(crtc);
+
 	return 0;
 }
 
@@ -1845,9 +1847,6 @@ via_iga1_crtc_prepare(struct drm_crtc *crtc)
 {
 	DRM_DEBUG_KMS("Entered %s.\n", __func__);
 
-	/* Turn off the cursor */
-	via_hide_cursor(crtc);
-
 	drm_crtc_vblank_off(crtc);
 
 	/* Blank the screen */
@@ -1861,9 +1860,6 @@ static void
 via_iga1_crtc_commit(struct drm_crtc *crtc)
 {
 	DRM_DEBUG_KMS("Entered %s.\n", __func__);
-
-	/* Turn on the cursor */
-	via_show_cursor(crtc);
 
 	drm_crtc_vblank_on(crtc);
 
@@ -2022,6 +2018,11 @@ via_iga1_crtc_mode_set(struct drm_crtc *crtc,
 
 	ret = via_iga1_crtc_mode_set_base(crtc, x, y, fb);
 
+	/* Specify the cursor pattern. */
+	via_cursor_address(crtc);
+
+	/* Turn off the cursor. */
+	via_hide_cursor(crtc);
 exit:
 	DRM_DEBUG_KMS("Exiting %s.\n", __func__);
 	return ret;
@@ -2128,9 +2129,6 @@ via_iga2_crtc_prepare(struct drm_crtc *crtc)
 {
 	DRM_DEBUG_KMS("Entered %s.\n", __func__);
 
-	/* Turn off the cursor */
-	via_hide_cursor(crtc);
-
 	drm_crtc_vblank_off(crtc);
 
 	/* Blank the screen */
@@ -2144,9 +2142,6 @@ static void
 via_iga2_crtc_commit(struct drm_crtc *crtc)
 {
 	DRM_DEBUG_KMS("Entered %s.\n", __func__);
-
-	/* Turn on the cursor */
-	via_show_cursor(crtc);
 
 	drm_crtc_vblank_on(crtc);
 
@@ -2334,6 +2329,11 @@ via_iga2_crtc_mode_set(struct drm_crtc *crtc,
 
 	ret = via_iga2_crtc_mode_set_base(crtc, x, y, fb);
 
+	/* Specify the cursor pattern. */
+	via_cursor_address(crtc);
+
+	/* Turn off the cursor. */
+	via_hide_cursor(crtc);
 exit:
 	DRM_DEBUG_KMS("Exiting %s.\n", __func__);
 	return ret;