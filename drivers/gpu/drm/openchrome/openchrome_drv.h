839508f2a378b3a49c59d6af7fe86f133e74a895
Kevin Brace <kevinbrace@gmx.com>
2018-07-16 18:54:53 -0700

    drm/openchrome: Version bumped to 3.0.85
    Improvement for Kconfig setting and garbled display fix for VIA
    Embedded EPIA-M830 mainboard (VX800 chipset).
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/openchrome_drv.h
+++ drivers/gpu/drm/openchrome/openchrome_drv.h
@@ -31,11 +31,11 @@
 #define DRIVER_AUTHOR		"OpenChrome Project"
 #define DRIVER_NAME		"openchrome"
 #define DRIVER_DESC		"OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE		"20180526"
+#define DRIVER_DATE		"20180716"
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		0
-#define DRIVER_PATCHLEVEL	84
+#define DRIVER_PATCHLEVEL	85
 #include <linux/module.h>
 
 #include "ttm/ttm_bo_api.h"
