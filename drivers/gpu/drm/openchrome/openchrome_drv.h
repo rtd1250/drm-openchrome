46caf7ce2b78b498240b4eb01d9889635e39531d
Kevin Brace <kevinbrace@gmx.com>
2018-10-11 20:29:39 -0700

    drm/openchrome: Version bumped to 3.0.87
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/openchrome_drv.h
+++ drivers/gpu/drm/openchrome/openchrome_drv.h
@@ -31,11 +31,11 @@
 #define DRIVER_AUTHOR		"OpenChrome Project"
 #define DRIVER_NAME		"openchrome"
 #define DRIVER_DESC		"OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE		"20180905"
+#define DRIVER_DATE		"20181011"
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		0
-#define DRIVER_PATCHLEVEL	86
+#define DRIVER_PATCHLEVEL	87
 #include <linux/module.h>
 
 #include "ttm/ttm_bo_api.h"
