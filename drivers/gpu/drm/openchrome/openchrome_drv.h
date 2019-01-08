b17f68f245743ce6b26ef22e28227dd8e0df7dce
Kevin Brace <kevinbrace@gmx.com>
2019-01-07 17:28:39 -0800

    drm/openchrome: Insert a new header file
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/openchrome_drv.h
+++ drivers/gpu/drm/openchrome/openchrome_drv.h
@@ -1,84 +1,96 @@
 /*
- * Copyright 2014 James Simmons <jsimmons@infradead.org>
- * Influenced by sample code from VIA Technologies and the radeon driver.
+ * Copyright © 2019 Kevin Brace
  *
- * Copyright 1998-2003 VIA Technologies, Inc. All Rights Reserved.
- * Copyright 2001-2003 S3 Graphics, Inc. All Rights Reserved.
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sub license,
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice (including
+ * the next paragraph) shall be included in all copies or substantial
+ * portions of the Software.
  *
- * The above copyright notice and this permission notice (including the
- * next paragraph) shall be included in all copies or substantial portions
- * of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHOR(S) OR COPYRIGHT HOLDER(S) BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  */
+/*
+ * Author(s):
+ *
+ * Kevin Brace <kevinbrace@gmx.com>
+ */
 
-#ifndef _VIA_DRV_H_
-#define _VIA_DRV_H_
+#ifndef _OPENCHROME_DRV_H
+#define _OPENCHROME_DRV_H
 
-#define DRIVER_AUTHOR		"OpenChrome Project"
-#define DRIVER_NAME		"openchrome"
-#define DRIVER_DESC		"OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE		"20190104"
 
-#define DRIVER_MAJOR		3
-#define DRIVER_MINOR		1
-#define DRIVER_PATCHLEVEL	0
 #include <linux/module.h>
+
+#include <video/vga.h>
+
+#include <drm/drmP.h>
+#include <drm/drm_global.h>
+#include <drm/drm_gem.h>
 
 #include "ttm/ttm_bo_api.h"
 #include "ttm/ttm_bo_driver.h"
 #include "ttm/ttm_placement.h"
-#include "ttm/ttm_memory.h"
-#include "ttm/ttm_module.h"
-#include "ttm/ttm_page_alloc.h"
-#include "ttm/ttm_tt.h"
 
-#include <drm/drmP.h>
-#include <drm/drm_gem.h>
 #include <drm/via_drm.h>
 
 #include "openchrome_display.h"
 #include "openchrome_regs.h"
 
+
+#define DRIVER_MAJOR		3
+#define DRIVER_MINOR		1
+#define DRIVER_PATCHLEVEL	0
+#define DRIVER_NAME		"openchrome"
+#define DRIVER_DESC		"OpenChrome DRM for VIA Technologies Chrome IGP"
+#define DRIVER_DATE		"20190104"
+#define DRIVER_AUTHOR		"OpenChrome Project"
+
+
 #define VIA_MM_ALIGN_SIZE	16
 
-#define DRM_FILE_PAGE_OFFSET (0x100000000ULL >> PAGE_SHIFT)
+#define DRM_FILE_PAGE_OFFSET	(0x100000000ULL >> PAGE_SHIFT)
+
 
 #define CLE266_REVISION_AX	0x0A
 #define CLE266_REVISION_CX	0x0C
+
 #define CX700_REVISION_700	0x0
 #define CX700_REVISION_700M	0x1
 #define CX700_REVISION_700M2	0x2
 
-/* For VT3353 */
-#define VX800_REVISION_A	0x0f
-#define VX800_REVISION_B0	0x10
-#define VX800_REVISION_B1	0x11
-#define VX800_REVISION_B2	0x12
+#define VIA_MEM_NONE		0x00
+#define VIA_MEM_SDR66		0x01
+#define VIA_MEM_SDR100		0x02
+#define VIA_MEM_SDR133		0x03
+#define VIA_MEM_DDR_200		0x04
+#define VIA_MEM_DDR_266		0x05
+#define VIA_MEM_DDR_333		0x06
+#define VIA_MEM_DDR_400		0x07
+#define VIA_MEM_DDR2_400	0x08
+#define VIA_MEM_DDR2_533	0x09
+#define VIA_MEM_DDR2_667	0x0A
+#define VIA_MEM_DDR2_800	0x0B
+#define VIA_MEM_DDR2_1066	0x0C
+#define VIA_MEM_DDR3_533	0x0D
+#define VIA_MEM_DDR3_667	0x0E
+#define VIA_MEM_DDR3_800	0x0F
+#define VIA_MEM_DDR3_1066	0x10
+#define VIA_MEM_DDR3_1333	0x11
+#define VIA_MEM_DDR3_1600	0x12
 
-/* For VT3409 */
-#define VX855_REVISION_A0	0x00
-#define VX855_REVISION_A1	0x01
-#define VX855_REVISION_A2	0x02
-
-/* For VT3410 */
-#define VX900_REVISION_A0	0x00
-#define VX900_REVISION_A1	0x01
-#define VX900_REVISION_A2	0x02
-#define VX900_REVISION_A3	0x03
 
 struct via_state {
 	struct vga_regset crt_regs[256];
@@ -181,20 +193,26 @@ struct via_device {
 	uint8_t saved_cr3e;
 	uint8_t saved_cr3f;
 
-	/* Due to the way VIA NanoBook reference design implemented
+	/*
+	 * Due to the way VIA NanoBook reference design implemented
 	 * the pin strapping settings, DRM needs to ignore them for
-	 * FP and DVI to be properly detected. */
+	 * FP and DVI to be properly detected.
+	 */
 	bool is_via_nanobook;
 
-	/* Quanta IL1 netbook has its FP connected to DVP1
+	/*
+	 * Quanta IL1 netbook has its FP connected to DVP1
 	 * rather than LVDS, hence, a special flag register
-	 * is needed for properly controlling its FP. */
+	 * is needed for properly controlling its FP.
+	 */
 	bool is_quanta_il1;
 
-	/* Samsung NC20 netbook has its FP connected to LVDS2
-	* rather than the more logical LVDS1, hence, a special
-	* flag register is needed for properly controlling its
-	* FP. */
+	/*
+	 * Samsung NC20 netbook has its FP connected to LVDS2
+	 * rather than the more logical LVDS1, hence, a special
+	 * flag register is needed for properly controlling its
+	 * FP.
+	 */
 	bool is_samsung_nc20;
 
 	bool analog_presence;
@@ -226,25 +244,6 @@ struct via_device {
 	u32 mapped_i2c_bus;
 };
 
-#define VIA_MEM_NONE		0x00
-#define VIA_MEM_SDR66		0x01
-#define VIA_MEM_SDR100		0x02
-#define VIA_MEM_SDR133		0x03
-#define VIA_MEM_DDR_200		0x04
-#define VIA_MEM_DDR_266		0x05
-#define VIA_MEM_DDR_333		0x06
-#define VIA_MEM_DDR_400		0x07
-#define VIA_MEM_DDR2_400	0x08
-#define VIA_MEM_DDR2_533	0x09
-#define VIA_MEM_DDR2_667	0x0A
-#define VIA_MEM_DDR2_800	0x0B
-#define VIA_MEM_DDR2_1066	0x0C
-#define VIA_MEM_DDR3_533	0x0D
-#define VIA_MEM_DDR3_667	0x0E
-#define VIA_MEM_DDR3_800	0x0F
-#define VIA_MEM_DDR3_1066	0x10
-#define VIA_MEM_DDR3_1333	0x11
-#define VIA_MEM_DDR3_1600	0x12
 
 /* VIA MMIO register access */
 #define VIA_BASE ((dev_priv->mmio))
@@ -323,4 +322,4 @@ void openchrome_transmitter_display_source(
 				struct via_device *dev_priv,
 				u32 di_port, int index);
 
-#endif
+#endif /* _OPENCHROME_DRV_H */
