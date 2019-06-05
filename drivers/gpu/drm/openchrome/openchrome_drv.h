9d7e0418894bb0f671eeb72138994528ad1e2ba5
Kevin Brace <kevinbrace@gmx.com>
2019-06-05 15:36:20 -0500

    drm/openchrome: Version bumped to 3.1.10
    Eliminated a few more compilation warnings that showed up.  At this
    point, there are no compilation warnings.
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/openchrome_drv.h
+++ drivers/gpu/drm/openchrome/openchrome_drv.h
@@ -51,10 +51,10 @@
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		1
-#define DRIVER_PATCHLEVEL	9
+#define DRIVER_PATCHLEVEL	10
 #define DRIVER_NAME		"openchrome"
 #define DRIVER_DESC		"OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE		"20190604"
+#define DRIVER_DATE		"20190605"
 #define DRIVER_AUTHOR		"OpenChrome Project"
 
 
