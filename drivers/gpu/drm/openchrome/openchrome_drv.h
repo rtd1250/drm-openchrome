45ea465fd9d56710758138a6b4c86c1f2592122b
Kevin Brace <kevinbrace@gmx.com>
2019-06-04 21:59:01 -0500

    drm/openchrome: Version bumped to 3.1.9
    Eliminated all known compilation warnings.
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/openchrome_drv.h
+++ drivers/gpu/drm/openchrome/openchrome_drv.h
@@ -51,7 +51,7 @@
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		1
-#define DRIVER_PATCHLEVEL	8
+#define DRIVER_PATCHLEVEL	9
 #define DRIVER_NAME		"openchrome"
 #define DRIVER_DESC		"OpenChrome DRM for VIA Technologies Chrome IGP"
 #define DRIVER_DATE		"20190604"
