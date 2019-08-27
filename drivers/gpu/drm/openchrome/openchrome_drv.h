4fa6d8c922a0c6d8c5ad8447be2f4a2e1036509f
Kevin Brace <kevinbrace@gmx.com>
2019-08-27 14:48:33 -0700

    drm/openchrome: Version bumped to 3.1.14
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/openchrome_drv.h
+++ drivers/gpu/drm/openchrome/openchrome_drv.h
@@ -51,10 +51,10 @@
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		1
-#define DRIVER_PATCHLEVEL	13
+#define DRIVER_PATCHLEVEL	14
 #define DRIVER_NAME		"openchrome"
 #define DRIVER_DESC		"OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE		"20190814"
+#define DRIVER_DATE		"20190827"
 #define DRIVER_AUTHOR		"OpenChrome Project"
 
 
