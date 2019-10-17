d693feb416eff609e51ebb996efacf89141e81fe
Kevin Brace <kevinbrace@gmx.com>
2019-10-16 18:01:57 -0700

    drm/openchrome: Version bumped to 3.1.18
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/openchrome_drv.h
+++ drivers/gpu/drm/openchrome/openchrome_drv.h
@@ -52,10 +52,10 @@
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		1
-#define DRIVER_PATCHLEVEL	17
+#define DRIVER_PATCHLEVEL	18
 #define DRIVER_NAME		"openchrome"
 #define DRIVER_DESC		"OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE		"20191014"
+#define DRIVER_DATE		"20191016"
 #define DRIVER_AUTHOR		"OpenChrome Project"
 
 
