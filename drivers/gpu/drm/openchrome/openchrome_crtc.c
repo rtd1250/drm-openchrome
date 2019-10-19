999f82852e6c643c9f81f595520b562c64a4a52b
Kevin Brace <kevinbrace@gmx.com>
2019-10-19 09:59:07 -0700

    drm/openchrome: Consolidate CRTC disable code
    It is being merged into openchrome_crtc_disable().
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/openchrome_crtc.c
+++ drivers/gpu/drm/openchrome/openchrome_crtc.c
@@ -1582,6 +1582,15 @@ drm_mode_crtc_load_lut(struct drm_crtc *crtc)
 	}
 }
 
+static void openchrome_crtc_disable(struct drm_crtc *crtc)
+{
+	DRM_DEBUG_KMS("Entered %s.\n", __func__);
+
+	crtc->helper_private->dpms(crtc, DRM_MODE_DPMS_OFF);
+
+	DRM_DEBUG_KMS("Exiting %s.\n", __func__);
+}
+
 static void openchrome_crtc_prepare(struct drm_crtc *crtc)
 {
 	DRM_DEBUG_KMS("Entered %s.\n", __func__);
@@ -2021,16 +2030,6 @@ via_iga1_crtc_dpms(struct drm_crtc *crtc, int mode)
 }
 
 static void
-via_iga1_crtc_disable(struct drm_crtc *crtc)
-{
-	DRM_DEBUG_KMS("Entered %s.\n", __func__);
-
-	via_iga1_crtc_dpms(crtc, DRM_MODE_DPMS_OFF);
-
-	DRM_DEBUG_KMS("Exiting %s.\n", __func__);
-}
-
-static void
 via_iga2_crtc_dpms(struct drm_crtc *crtc, int mode)
 {
 	struct openchrome_drm_private *dev_private =
@@ -2062,20 +2061,10 @@ via_iga2_crtc_dpms(struct drm_crtc *crtc, int mode)
 	DRM_DEBUG_KMS("Exiting %s.\n", __func__);
 }
 
-static void
-via_iga2_crtc_disable(struct drm_crtc *crtc)
-{
-	DRM_DEBUG_KMS("Entered %s.\n", __func__);
-
-	via_iga2_crtc_dpms(crtc, DRM_MODE_DPMS_OFF);
-
-	DRM_DEBUG_KMS("Exiting %s.\n", __func__);
-}
-
 static const struct
 drm_crtc_helper_funcs openchrome_iga1_drm_crtc_helper_funcs = {
 	.dpms = via_iga1_crtc_dpms,
-	.disable = via_iga1_crtc_disable,
+	.disable = openchrome_crtc_disable,
 	.prepare = openchrome_crtc_prepare,
 	.commit = openchrome_crtc_commit,
 	.mode_fixup = openchrome_crtc_mode_fixup,
@@ -2086,7 +2075,7 @@ drm_crtc_helper_funcs openchrome_iga1_drm_crtc_helper_funcs = {
 static const struct
 drm_crtc_helper_funcs openchrome_iga2_drm_crtc_helper_funcs = {
 	.dpms = via_iga2_crtc_dpms,
-	.disable = via_iga2_crtc_disable,
+	.disable = openchrome_crtc_disable,
 	.prepare = openchrome_crtc_prepare,
 	.commit = openchrome_crtc_commit,
 	.mode_fixup = openchrome_crtc_mode_fixup,
