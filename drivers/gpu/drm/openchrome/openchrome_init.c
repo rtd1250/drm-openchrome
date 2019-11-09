41065a3970b44a17d0dad4477bc30d56ab2a57e5
Kevin Brace <kevinbrace@gmx.com>
2019-11-08 17:45:46 -0800

    drm/openchrome: Eliminate 3D engine initialization code
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/openchrome_init.c
+++ drivers/gpu/drm/openchrome/openchrome_init.c
@@ -1299,147 +1299,14 @@ static void via_init_2d(
 		VIA_WRITE(0x60, 0x0);
 }
 
-static void via_init_3d(
-		struct openchrome_drm_private *dev_private)
-{
-	unsigned long texture_stage;
-	int i;
-
-	VIA_WRITE(VIA_REG_TRANSET, 0x00010000);
-	for (i = 0; i <= 0x9A; i++)
-		VIA_WRITE(VIA_REG_TRANSPACE, i << 24);
-
-	/* guardband clipping default setting */
-	VIA_WRITE(VIA_REG_TRANSPACE, (0x88 << 24) | 0x00001ed0);
-	VIA_WRITE(VIA_REG_TRANSPACE, (0x89 << 24) | 0x00000800);
-
-	/* Initial Texture Stage Setting */
-	for (texture_stage = 0; texture_stage <= 0xf; texture_stage++) {
-		VIA_WRITE(VIA_REG_TRANSET, (0x00020000 | 0x00000000 |
-					(texture_stage & 0xf) << 24));
-		for (i = 0 ; i <= 0x30 ; i++)
-			VIA_WRITE(VIA_REG_TRANSPACE, i << 24);
-	}
-
-	/* Initial Texture Sampler Setting */
-	for (texture_stage = 0; texture_stage <= 0xf; texture_stage++) {
-		VIA_WRITE(VIA_REG_TRANSET, (0x00020000 | 0x20000000 |
-					(texture_stage & 0x10) << 24));
-		for (i = 0; i <= 0x36; i++)
-			VIA_WRITE(VIA_REG_TRANSPACE, i << 24);
-	}
-
-	VIA_WRITE(VIA_REG_TRANSET, (0x00020000 | 0xfe000000));
-	for (i = 0 ; i <= 0x13 ; i++)
-		VIA_WRITE(VIA_REG_TRANSPACE, i << 24);
-
-	/* degamma table */
-	VIA_WRITE(VIA_REG_TRANSET, (0x00030000 | 0x15000000));
-	VIA_WRITE(VIA_REG_TRANSPACE, (0x40000000 | (30 << 20) | (15 << 10) | (5)));
-	VIA_WRITE(VIA_REG_TRANSPACE, ((119 << 20) | (81 << 10) | (52)));
-	VIA_WRITE(VIA_REG_TRANSPACE, ((283 << 20) | (219 << 10) | (165)));
-	VIA_WRITE(VIA_REG_TRANSPACE, ((535 << 20) | (441 << 10) | (357)));
-	VIA_WRITE(VIA_REG_TRANSPACE, ((119 << 20) | (884 << 20) | (757 << 10) | (640)));
-
-	/* gamma table */
-	VIA_WRITE(VIA_REG_TRANSET, (0x00030000 | 0x17000000));
-	VIA_WRITE(VIA_REG_TRANSPACE, (0x40000000 | (13 << 20) | (13 << 10) | (13)));
-	VIA_WRITE(VIA_REG_TRANSPACE, (0x40000000 | (26 << 20) | (26 << 10) | (26)));
-	VIA_WRITE(VIA_REG_TRANSPACE, (0x40000000 | (39 << 20) | (39 << 10) | (39)));
-	VIA_WRITE(VIA_REG_TRANSPACE, ((51 << 20) | (51 << 10) | (51)));
-	VIA_WRITE(VIA_REG_TRANSPACE, ((71 << 20) | (71 << 10) | (71)));
-	VIA_WRITE(VIA_REG_TRANSPACE, (87 << 20) | (87 << 10) | (87));
-	VIA_WRITE(VIA_REG_TRANSPACE, (113 << 20) | (113 << 10) | (113));
-	VIA_WRITE(VIA_REG_TRANSPACE, (135 << 20) | (135 << 10) | (135));
-	VIA_WRITE(VIA_REG_TRANSPACE, (170 << 20) | (170 << 10) | (170));
-	VIA_WRITE(VIA_REG_TRANSPACE, (199 << 20) | (199 << 10) | (199));
-	VIA_WRITE(VIA_REG_TRANSPACE, (246 << 20) | (246 << 10) | (246));
-	VIA_WRITE(VIA_REG_TRANSPACE, (284 << 20) | (284 << 10) | (284));
-	VIA_WRITE(VIA_REG_TRANSPACE, (317 << 20) | (317 << 10) | (317));
-	VIA_WRITE(VIA_REG_TRANSPACE, (347 << 20) | (347 << 10) | (347));
-	VIA_WRITE(VIA_REG_TRANSPACE, (373 << 20) | (373 << 10) | (373));
-	VIA_WRITE(VIA_REG_TRANSPACE, (398 << 20) | (398 << 10) | (398));
-	VIA_WRITE(VIA_REG_TRANSPACE, (442 << 20) | (442 << 10) | (442));
-	VIA_WRITE(VIA_REG_TRANSPACE, (481 << 20) | (481 << 10) | (481));
-	VIA_WRITE(VIA_REG_TRANSPACE, (517 << 20) | (517 << 10) | (517));
-	VIA_WRITE(VIA_REG_TRANSPACE, (550 << 20) | (550 << 10) | (550));
-	VIA_WRITE(VIA_REG_TRANSPACE, (609 << 20) | (609 << 10) | (609));
-	VIA_WRITE(VIA_REG_TRANSPACE, (662 << 20) | (662 << 10) | (662));
-	VIA_WRITE(VIA_REG_TRANSPACE, (709 << 20) | (709 << 10) | (709));
-	VIA_WRITE(VIA_REG_TRANSPACE, (753 << 20) | (753 << 10) | (753));
-	VIA_WRITE(VIA_REG_TRANSPACE, (794 << 20) | (794 << 10) | (794));
-	VIA_WRITE(VIA_REG_TRANSPACE, (832 << 20) | (832 << 10) | (832));
-	VIA_WRITE(VIA_REG_TRANSPACE, (868 << 20) | (868 << 10) | (868));
-	VIA_WRITE(VIA_REG_TRANSPACE, (902 << 20) | (902 << 10) | (902));
-	VIA_WRITE(VIA_REG_TRANSPACE, (934 << 20) | (934 << 10) | (934));
-	VIA_WRITE(VIA_REG_TRANSPACE, (966 << 20) | (966 << 10) | (966));
-	VIA_WRITE(VIA_REG_TRANSPACE, (996 << 20) | (996 << 10) | (996));
-
-	/* Initialize INV_ParaSubType_TexPa */
-	VIA_WRITE(VIA_REG_TRANSET, (0x00030000 | 0x00000000));
-	for (i = 0; i < 16; i++)
-		VIA_WRITE(VIA_REG_TRANSPACE, 0x00000000);
-
-	/* Initialize INV_ParaSubType_4X4Cof */
-	VIA_WRITE(VIA_REG_TRANSET, (0x00030000 | 0x11000000));
-	for (i = 0; i < 32; i++)
-		VIA_WRITE(VIA_REG_TRANSPACE, 0x00000000);
-
-	/* Initialize INV_ParaSubType_StipPal */
-	VIA_WRITE(VIA_REG_TRANSET, (0x00030000 | 0x14000000));
-	for (i = 0; i < (5 + 3); i++)
-		VIA_WRITE(VIA_REG_TRANSPACE, 0x00000000);
-
-	/* primitive setting & vertex format */
-	VIA_WRITE(VIA_REG_TRANSET, (0x00040000));
-	for (i = 0; i <= 0x62; i++)
-		VIA_WRITE(VIA_REG_TRANSPACE, (i << 24));
-
-	/* c2s clamping value for screen coordinate */
-	VIA_WRITE(VIA_REG_TRANSPACE, (0x50 << 24) | 0x00000000);
-	VIA_WRITE(VIA_REG_TRANSPACE, (0x51 << 24) | 0x00000000);
-	VIA_WRITE(VIA_REG_TRANSPACE, (0x52 << 24) | 0x00147fff);
-
-	/* ParaType 0xFE - Configure and Misc Setting */
-	VIA_WRITE(VIA_REG_TRANSET, (0x00fe0000));
-	for (i = 0; i <= 0x47; i++)
-		VIA_WRITE(VIA_REG_TRANSPACE, (i << 24));
-
-	/* ParaType 0x11 - Frame Buffer Auto-Swapping and Command Regulator */
-	VIA_WRITE(VIA_REG_TRANSET, (0x00110000));
-	for (i = 0; i <= 0x20; i++)
-		VIA_WRITE(VIA_REG_TRANSPACE, (i << 24));
-
-	VIA_WRITE(VIA_REG_TRANSET, 0x00fe0000);
-	VIA_WRITE(VIA_REG_TRANSPACE, 0x4000840f);
-	VIA_WRITE(VIA_REG_TRANSPACE, 0x47000404);
-	VIA_WRITE(VIA_REG_TRANSPACE, 0x44000000);
-	VIA_WRITE(VIA_REG_TRANSPACE, 0x46000005);
-
-	/* setting Misconfig */
-	VIA_WRITE(VIA_REG_TRANSET, 0x00fe0000);
-	VIA_WRITE(VIA_REG_TRANSPACE, 0x00001004);
-	VIA_WRITE(VIA_REG_TRANSPACE, 0x08000249);
-	VIA_WRITE(VIA_REG_TRANSPACE, 0x0a0002c9);
-	VIA_WRITE(VIA_REG_TRANSPACE, 0x0b0002fb);
-	VIA_WRITE(VIA_REG_TRANSPACE, 0x0c000000);
-	VIA_WRITE(VIA_REG_TRANSPACE, 0x0d0002cb);
-	VIA_WRITE(VIA_REG_TRANSPACE, 0x0e000009);
-	VIA_WRITE(VIA_REG_TRANSPACE, 0x10000049);
-	VIA_WRITE(VIA_REG_TRANSPACE, 0x110002ff);
-	VIA_WRITE(VIA_REG_TRANSPACE, 0x12000008);
-	VIA_WRITE(VIA_REG_TRANSPACE, 0x130002db);
-}
-
 /* This function does:
  * 1. Command buffer allocation
- * 2. hw engine intialization:2D;3D
+ * 2. hw engine intialization:2D
  */
 void via_engine_init(struct drm_device *dev)
 {
 	struct openchrome_drm_private *dev_private = dev->dev_private;
 
-	/* initial engines */
+	/* initial engine */
 	via_init_2d(dev_private, dev->pdev->device);
-	via_init_3d(dev_private);
 }
