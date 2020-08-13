1f28fa3d000f6f0faff9227969dd7753c3355de5
Kevin Brace <kevinbrace@gmx.com>
2020-08-12 18:10:42 -0700

    drm/openchrome: Version bumped to 3.3.0
    Added a new OpenChrome DRM uAPI service (DRM_OPENCHROME_GEM_UNMAP), so
    the OpenChrome DRM minor version number will have to be incremented by 1.
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/openchrome_drv.h
+++ drivers/gpu/drm/openchrome/openchrome_drv.h
@@ -60,11 +60,11 @@
 
 
 #define DRIVER_MAJOR		3
-#define DRIVER_MINOR		2
+#define DRIVER_MINOR		3
 #define DRIVER_PATCHLEVEL	0
 #define DRIVER_NAME		"openchrome"
 #define DRIVER_DESC		"OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE		"20200810"
+#define DRIVER_DATE		"20200812"
 #define DRIVER_AUTHOR		"OpenChrome Project"
 
 
