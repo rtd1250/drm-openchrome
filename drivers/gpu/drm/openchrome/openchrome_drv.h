ce6d16aee96a0f1cf2f537cd66c53ff0a27d7191
Kevin Brace <kevinbrace@gmx.com>
2020-09-14 14:42:27 -0700

    drm/openchrome: Version bumped to 3.3.4
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/openchrome_drv.h
+++ drivers/gpu/drm/openchrome/openchrome_drv.h
@@ -61,10 +61,10 @@
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		3
-#define DRIVER_PATCHLEVEL	3
+#define DRIVER_PATCHLEVEL	4
 #define DRIVER_NAME		"openchrome"
 #define DRIVER_DESC		"OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE		"20200910"
+#define DRIVER_DATE		"20200914"
 #define DRIVER_AUTHOR		"OpenChrome Project"
 
 
