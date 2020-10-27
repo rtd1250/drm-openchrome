9e184ca52b9673d31aa68a48e6f3dfa0bde02746
Kevin Brace <kevinbrace@gmx.com>
2020-10-27 15:06:17 -0700

    drm/openchrome: Base fb_info struct’s size on the number of pages
    This is for fixed screen size (fix.smem_len) and current screen size
    (screen_size) members.
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/openchrome_fb.c
+++ drivers/gpu/drm/openchrome/openchrome_fb.c
@@ -211,10 +211,12 @@ openchrome_fb_probe(struct drm_fb_helper *helper,
 
 	info->fix.smem_start = openchrome_fb->bo->ttm_bo.mem.bus.base +
 				openchrome_fb->bo->ttm_bo.mem.bus.offset;
-	info->fix.smem_len = size;
+	info->fix.smem_len = openchrome_fb->bo->ttm_bo.mem.num_pages <<
+							PAGE_SHIFT;
 	info->screen_base = ttm_kmap_obj_virtual(
 				&openchrome_fb->bo->kmap, &is_iomem);
-	info->screen_size = size;
+	info->screen_size = openchrome_fb->bo->ttm_bo.mem.num_pages <<
+							PAGE_SHIFT;
 
 	info->apertures->ranges[0].base = dev_private->vram_start;
 	info->apertures->ranges[0].size = dev_private->vram_size;
