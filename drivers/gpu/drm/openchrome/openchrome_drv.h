7b2b76afcf00533f089e6c306f1eb430e09d4523
Kevin Brace <kevinbrace@gmx.com>
2020-11-27 13:20:37 -0800

    drm/openchrome: Version bumped to 3.4.5
    Fix for resume failure on Foxconn 400M01-G-6L mainboard (VIA
    Technologies KM400 chipset).
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/openchrome_drv.h
+++ drivers/gpu/drm/openchrome/openchrome_drv.h
@@ -61,10 +61,10 @@
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		4
-#define DRIVER_PATCHLEVEL	4
+#define DRIVER_PATCHLEVEL	5
 #define DRIVER_NAME		"openchrome"
 #define DRIVER_DESC		"OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE		"20201123"
+#define DRIVER_DATE		"20201127"
 #define DRIVER_AUTHOR		"OpenChrome Project"
 
 
