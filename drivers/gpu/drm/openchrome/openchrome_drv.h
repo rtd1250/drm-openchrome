ed3211a6fc8111854149dcdef4b4492963c50668
Kevin Brace <kevinbrace@gmx.com>
2022-01-25 16:35:17 -0600

    drm/openchrome: Version bumped to 3.4.16
    Previous patch level did not completely eliminate openchrome_fb.c.
    This one does.
    
    Signed-off-by: Kevin Brace <kevinbrace@gmx.com>

--- drivers/gpu/drm/openchrome/openchrome_drv.h
+++ drivers/gpu/drm/openchrome/openchrome_drv.h
@@ -60,10 +60,10 @@
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		4
-#define DRIVER_PATCHLEVEL	15
+#define DRIVER_PATCHLEVEL	16
 #define DRIVER_NAME		"openchrome"
 #define DRIVER_DESC		"OpenChrome DRM for VIA Technologies Chrome IGP"
-#define DRIVER_DATE		"20220121"
+#define DRIVER_DATE		"20220125"
 #define DRIVER_AUTHOR		"OpenChrome Project"
 
 
