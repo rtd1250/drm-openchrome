72eb2ccdcd52ab717f454681c17403a8e5ab2540
Kevin Brace <kevinbrace@bracecomputerlab.com>
2022-06-18 18:07:11 -0500

    drm/via: Remove some comments from via_pll.c
    It does not appear that PLL clock frequency calculation code was
    borrowed from viafb, so delete the reference to it.  At the same
    time, delete the reference to OpenChrome DDX at the beginning of
    the file.
    
    Signed-off-by: Kevin Brace <kevinbrace@bracecomputerlab.com>

--- drivers/gpu/drm/via/via_pll.c
+++ drivers/gpu/drm/via/via_pll.c
@@ -1,9 +1,5 @@
 /*
  * Copyright 2012 James Simmons <jsimmons@infradead.org>
- *
- * Based on code for the viafb driver.
- * UniChrome PLL parameters calculation code borrowed from OpenChrome DDX
- * device driver.
  * Copyright 1998-2008 VIA Technologies, Inc. All Rights Reserved.
  * Copyright 2001-2008 S3 Graphics, Inc. All Rights Reserved.
  *
