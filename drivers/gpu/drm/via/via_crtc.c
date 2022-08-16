b82a3f9a7152521d9b88ae073716d2d2489bee6c
Kevin Brace <kevinbrace@bracecomputerlab.com>
2022-08-15 18:22:50 -0700

    drm: Drop drm_framebuffer.h from drm_crtc.h
    Based on commit 720cf96 (drm: Drop drm_framebuffer.h from drm_crtc.h).
    
    Signed-off-by: Kevin Brace <kevinbrace@bracecomputerlab.com>

--- drivers/gpu/drm/via/via_crtc.c
+++ drivers/gpu/drm/via/via_crtc.c
@@ -37,6 +37,7 @@
 #include <drm/drm_crtc.h>
 #include <drm/drm_crtc_helper.h>
 #include <drm/drm_fourcc.h>
+#include <drm/drm_framebuffer.h>
 #include <drm/drm_gem.h>
 #include <drm/drm_mode.h>
 #include <drm/drm_modeset_helper_vtables.h>
