ded8ec0d2643f48fe857df54a64d22e4aef8ab95
Kevin Brace <kevinbrace@bracecomputerlab.com>
2022-09-08 16:37:54 -0700

    drm/via: Version bumped to 3.6.0
    Signed-off-by: Kevin Brace <kevinbrace@bracecomputerlab.com>

--- drivers/gpu/drm/via/via_drv.h
+++ drivers/gpu/drm/via/via_drv.h
@@ -49,11 +49,11 @@
 
 
 #define DRIVER_MAJOR		3
-#define DRIVER_MINOR		5
-#define DRIVER_PATCHLEVEL	8
+#define DRIVER_MINOR		6
+#define DRIVER_PATCHLEVEL	0
 #define DRIVER_NAME		"via"
 #define DRIVER_DESC		"OpenChrome DRM for VIA Technologies Chrome"
-#define DRIVER_DATE		"20220728"
+#define DRIVER_DATE		"20220908"
 #define DRIVER_AUTHOR		"OpenChrome Project"
 
 
