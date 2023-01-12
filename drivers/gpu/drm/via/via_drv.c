d0ac9b6e67518e05217deea93681a4df1f55d044
Kevin Brace <kevinbrace@bracecomputerlab.com>
2023-01-12 13:00:00 -0600

    drm/fb-helper: Move generic fbdev emulation into separate source file
    Based on commit 8ab59da (drm/fb-helper: Move generic fbdev emulation
    into separate source file).
    
    Signed-off-by: Kevin Brace <kevinbrace@bracecomputerlab.com>

--- drivers/gpu/drm/via/via_drv.c
+++ drivers/gpu/drm/via/via_drv.c
@@ -30,7 +30,7 @@
 
 #include <drm/drm_aperture.h>
 #include <drm/drm_drv.h>
-#include <drm/drm_fb_helper.h>
+#include <drm/drm_fbdev_generic.h>
 #include <drm/drm_file.h>
 #include <drm/drm_gem.h>
 #include <drm/drm_ioctl.h>
